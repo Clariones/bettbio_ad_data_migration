dc3f9999fa77d0241fad6fc78ef3d0b0ccda7b43
clariones <clariones@github.com>
2017-10-25 12:41:51 +0800

    update according to bettbio_ad_v2.2

--- v1v2_to_v3/eclipse_prj/src/com/skynet/bettbioadv2/dboperator/BaseDbOperator.java
+++ v1v2_to_v3/eclipse_prj/src/com/skynet/bettbioadv2/dboperator/BaseDbOperator.java
@@ -12,9 +12,11 @@ import org.springframework.jdbc.core.RowMapper;
 public abstract class BaseDbOperator {
 	protected DataSource dataSource;
 	protected JdbcTemplate jdbcTemplateObject;
+
 	public DataSource getDataSource() {
 		return dataSource;
 	}
+
 	public void setDataSource(DataSource dataSource) {
 		this.dataSource = dataSource;
 		this.jdbcTemplateObject = new JdbcTemplate(this.dataSource);
@@ -22,46 +24,57 @@ public abstract class BaseDbOperator {
 		jdbcTemplateObject.setFetchSize(1000);
 		jdbcTemplateObject.setQueryTimeout(10000);
 	}
+
 	public JdbcTemplate getJdbcTemplateObject() {
 		return jdbcTemplateObject;
 	}
+
 	public void setJdbcTemplateObject(JdbcTemplate jdbcTemplateObject) {
 		this.jdbcTemplateObject = jdbcTemplateObject;
 	}
+
 	protected void executePlainSql(String sql) {
 		getJdbcTemplateObject().execute(sql);
 	}
+
 	protected int executeUpdateSql(String description, String sql) {
-//		System.out.print(sql);
+		// System.out.print(sql);
 		Object[] args = null;
 		if (description != null) {
-			System.out.print(description+"...");
+			System.out.print(description + "...");
 		}
-		int n = getJdbcTemplateObject().update(sql, args);
-		if (description != null) {
-			System.out.println(" "+n+" rows done");
+		try {
+			int n = getJdbcTemplateObject().update(sql, args);
+			if (description != null) {
+				System.out.println(" " + n + " rows done");
+			}
+			return n;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
 		}
-		return n;
 	}
+
 	protected void executeUpdateSql(String sql, List<? extends Object> params) {
 		Object[] paramArray = params.toArray();
 		getJdbcTemplateObject().update(sql, paramArray);
-		
+
 	}
+
 	public <T> T queryForOneResult(String sql, List<Object> params, Class<T> requiredType) {
-		if (params != null){
+		if (params != null) {
 			return getJdbcTemplateObject().queryForObject(sql, params.toArray(), requiredType);
 		}
 		return getJdbcTemplateObject().queryForObject(sql, null, requiredType);
 	}
-	
+
 	public <T> List<T> queryObjectList(String sql, List<Object> params, Class<T> requiredType) {
-		
+
 		Object[] args = null;
-		if (params != null){
+		if (params != null) {
 			args = params.toArray();
 		}
 		return getJdbcTemplateObject().queryForList(sql, args, requiredType);
 	}
-	
+
 }
